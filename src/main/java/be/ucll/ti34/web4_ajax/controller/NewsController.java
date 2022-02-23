1aa3cc237e6d2f87bc2ab60de3573ece1719023a
ikbenignace <ikbenignace@live.be>
2022-02-23 17:08:37 +0100

    correct wireframe

--- src/main/java/be/ucll/ti34/web4_ajax/controller/NewsController.java
+++ src/main/java/be/ucll/ti34/web4_ajax/controller/NewsController.java
@@ -5,18 +5,13 @@ import be.ucll.ti34.web4_ajax.model.News;
 import be.ucll.ti34.web4_ajax.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -35,6 +30,12 @@ public class NewsController {
     @ResponseBody
     public List<News> all(){
         return repository.findAll(Sort.by(Sort.Direction.DESC, "createdAt"));
+    }
+
+    @GetMapping("/news/{id}")
+    @ResponseBody
+    public Optional<News> getById(@PathVariable("id") long id){
+        return repository.findById(id);
     }
 
     @PostMapping("/news")
